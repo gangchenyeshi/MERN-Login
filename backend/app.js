bb0a2eca1dd47d86320439b6c5aa7deb372a933b
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-05-19 16:33:03 +0200

    remove node_modules

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,24 @@
 const express = require ("express");
+
 const app = express();
 
+
+// MIDDLEWARE
+// Why middleware?
+// if user want to access the about page, middleware will check the user login or not, without login then middleware is entering
+const middleware = (req, res, next) => {
+    console.log("Hello Middleware");
+    next();
+}
+// middleware();
 
 
 // ROUTING
 app.get('/', (req, res) => {
     res.send("Hello Home page from Server");
 });
-app.get('/about', (req, res) => {
+app.get('/about', middleware,(req, res) => {
+    console.log("Hello Middleware from About");
     res.send("Hello about page from Server");
 });
 app.get('/contact', (req, res) => {
